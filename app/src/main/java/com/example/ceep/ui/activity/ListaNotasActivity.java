ad3e26685962b9144f49b223df8e396250642ffe
Leandro <lealmeids@gmail.com>
2021-02-06 15:29:33 -0400

    Adicionando formulário de nota

--- app/src/main/java/com/example/ceep/ui/activity/ListaNotasActivity.java
+++ app/src/main/java/com/example/ceep/ui/activity/ListaNotasActivity.java
@@ -1,6 +1,9 @@
 package com.example.ceep.ui.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -21,6 +24,16 @@ public class ListaNotasActivity extends AppCompatActivity {
         setContentView(R.layout.activity_lista_notas);
         List<Nota> todasNotas = notasDeExemplo();
         configuraRecyclerView(todasNotas);
+
+        TextView botaoInsereNota = findViewById(R.id.lista_notas_insere_nota);
+        botaoInsereNota.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent iniciaFormularioNota = new Intent(ListaNotasActivity.this,
+                        FormularioNotaActivity.class);
+                startActivity(iniciaFormularioNota);
+            }
+        });
     }
 
     private List<Nota> notasDeExemplo() {
